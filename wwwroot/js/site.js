1663fcc479d52945102710887ddb83fcb8214878
Km-Ashwani <ashu893183@gmail.com>
2025-05-16 09:18:14 +0530

    make responsive header

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 80) {
+            $('header').addClass("sticky");
+        }
+        else {
+            $('header').removeClass("sticky");
+        }
+    })
+})
+
+//var parallax = $('.parallax');
+//var scrollposition = $(this).scrollTop();
+//parallax.css("transform", "translateY(" + scrollposition * 0.5 + "px" + ")");
